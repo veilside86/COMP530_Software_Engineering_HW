16840c0590c7555a02271a384d6bc3a954798261
Junseok-Hur <78327059+Junseok-Hur@users.noreply.github.com>
2023-04-28 19:44:28 -0400

    update source code to github

---
+++ main.py
@@ -0,0 +1,63 @@
+import sys
+
+
+def calculate_total(state: str, records: list) -> float:
+    """
+    DE: no sales tax 
+    NJ: Sales tax rate is 6.6% 
+    PA: Sales tax rate is 6% (we'll ignore the local tax) 
+    WIC Eligible food is tax exempt in all three states 
+    Clothing is exempt from in PA, and NJ except Fur clothing is taxable normally in NJ,
+    we'll call it taxable if the string fur appears in a clothing item name Everything 
+    else is covered under the default tax rates
+    """
+    # tax rates
+    tax_rates = {'NJ': 1.066, 'PA': 1.06, 'DE': 0}
+    # tax exemptions
+    tax_exempt = ['Wic Eligible food', 'Clothing']
+    # tax exemption clothing types in NJ
+    taxable_clothing = {'Fur'}
+    total = 0
+
+    # Calculate subtotal
+    for record in records:
+        item_type = record['type']
+        price = record['price']
+
+        try:
+            if price < 0 or price == 0 or price > sys.maxsize:
+                raise ValueError("Incorrect price")
+        except ValueError:
+            return None
+
+        match state:
+            case 'DE':
+                total = round(total + price, 2)
+            case 'PA':
+                if item_type in tax_exempt:
+                    total = round(total + price, 2)
+                else:
+                    total = round(total + price * tax_rates[state], 2)
+            case 'NJ':
+                if item_type in tax_exempt:
+                    if item_type == 'Wic Eligible food':
+                        total = round(total + price, 2)
+                    elif item_type == 'Clothing' and any(taxable in record['name'] for taxable in taxable_clothing):
+                        total = round(total + price * tax_rates[state], 2)
+                    else:
+                        total = round(total + price, 2)
+                else:
+                    total = round(total + price * tax_rates[state], 2)
+                    
+    return total
+
+
+if __name__ == '__main__':
+    records = [
+        {'name': 'Fox Fur Coat', 'price': 1984.99, 'type': 'Clothing'},
+        {'name': 'Tuttle Neck Sweater', 'price': 39.99, 'type': 'Clothing'},
+    ]
+
+    print(calculate_total('NJ', records))
+    print(calculate_total('PA', records))
+    print(calculate_total('DE', records))
---
+++ tests/test_main.py
@@ -0,0 +1,84 @@
+from main import calculate_total
+import pytest
+import sys
+
+
+def test_all_items():
+    records = [
+        {'name': 'Apples', 'price': 1.99, 'type': 'Wic Eligible food'},
+        {'name': 'Shirt', 'price': 19.99, 'type': 'Clothing'},
+        {'name': 'Book', 'price': 9.99, 'type': 'Everything else'},
+        {'name': 'Fox Fur Coat', 'price': 1984.99, 'type': 'Clothing'},
+    ]
+    assert calculate_total('NJ', records) == 2148.63
+    assert calculate_total('PA', records) == 2017.56
+    assert calculate_total('DE', records) == 2016.96
+
+
+# testing empty lists
+def test_no_items():
+    assert calculate_total('NJ', []) == 0
+    assert calculate_total('PA', []) == 0
+    assert calculate_total('DE', []) == 0
+
+
+# testing incorrect types
+def test_diff_types():
+    records = [
+        {'name': 'Speaker', 'price': 199.99, 'type': 'Electronic'},
+        {'name': 'Laptop', 'price': 1999.99, 'type': 'awefawefg'},
+        {'name': 'Amazon Echo', 'price': 49.99, 'type': 'casergrgc'},
+    ]
+    assert calculate_total('NJ', records) == 2398.47
+    assert calculate_total('PA', records) == 2384.97
+    assert calculate_total('DE', records) == 2249.97
+
+
+# testing negative prices
+def test_negative_prices():
+    records = [
+        {'name': 'speaker', 'price': -149.99, 'type': 'Electronic'},
+        {'name': 'laptop', 'price': 1999.99, 'type': 'Electronic'},
+        {'name': 'amazon echo', 'price': -49.99, 'type': 'Electronic'},
+    ]
+    with pytest.raises(ValueError):
+        for record in records:
+            price = record['price']
+            if price < 0 or price == 0 or price > sys.maxsize:
+                raise ValueError("Incorrect price")
+    assert calculate_total('NJ', records) is None
+    assert calculate_total('PA', records) is None
+    assert calculate_total('DE', records) is None
+
+
+def test_tax_exempt_items():
+    records = [
+        {'name': 'Apples', 'price': 1.99, 'type': 'Wic Eligible food'},
+        {'name': 'Shirt', 'price': 19.99, 'type': 'Clothing'},
+        {'name': 'Book', 'price': 9.99, 'type': 'Everything else'},
+    ]
+    assert calculate_total('NJ', records) == 32.63
+    assert calculate_total('PA', records) == 32.57
+    assert calculate_total('DE', records) == 31.97
+
+
+# testing for fur vs non-fur clothes
+def test_taxable_clothing_items():
+    records = [
+        {'name': 'Fox Fur Coat', 'price': 1984.99, 'type': 'Clothing'},
+        {'name': 'Tuttle Neck Sweater', 'price': 39.99, 'type': 'Clothing'},
+    ]
+    assert calculate_total('NJ', records) == 2155.99
+    assert calculate_total('PA', records) == 2024.98
+    assert calculate_total('DE', records) == 2024.98
+
+
+def test_default_tax_rate_items():
+    records = [
+        {'name': 'TV', 'price': 599.99, 'type': 'Everything else'},
+        {'name': 'Jeans', 'price': 49.99, 'type': 'Clothing'},
+        {'name': 'Bananas', 'price': 0.99, 'type': 'Wic Eligible food'},
+    ]
+    assert calculate_total('NJ', records) == 690.57
+    assert calculate_total('PA', records) == 686.97
+    assert calculate_total('DE', records) == 650.97
